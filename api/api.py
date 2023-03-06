6e56a2af5696536b0844fff99cfc4fcb6746edc2
Jaakko Ihanamäki <84900656+jihanm@users.noreply.github.com>
2023-03-06 10:16:45 +0200

    Fix getTransferData endpoint 404 type (#22)

--- api/api.py
+++ api/api.py
@@ -28,7 +28,7 @@ def get_foul_data(request, foul_number: int = 113148427, register_number: str = 
     return req.json()
 
 
-@router.get('/getTransferData', response={200: TransferDataResponse, 404: Any}, tags=['PASI'])
+@router.get('/getTransferData', response={200: TransferDataResponse, 404: None}, tags=['PASI'])
 def get_transfer_data(request, transfer_number: int = 11720143, register_number: str = "HKR-999"):
     """
     Retrieve transfer data from PASI by transfer number and register number
